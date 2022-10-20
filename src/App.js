5e1d4f4f06b48d1976180cda0a7d1747f64a0c25
MahtabBukhari <git config --global user.email you@example.com>
2022-10-20 17:38:56 +0500

    setup pages and layout files structure that is best way

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import Footer from './layouts/footer';
+import Header from './layouts/header';
+import Home from './pages/Home';
 import './styles/App.css';
 
 
 const App = () => {
   return (
     <div>
-  app
-    
+       <Header/>
+        <Home/>
+       <Footer/>
     </div>
   )
 }
---
+++ src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './styles.css'
+
+const Home = () => {
+  return (
+    <div>
+        <div>top of list</div>
+        <div>list</div>
+    </div>
+  )
+}
+
+export default Home

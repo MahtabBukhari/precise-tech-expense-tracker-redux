527d8fd9c4d68885fb767d12fba8e380c569ff94
MahtabBukhari <git config --global user.email you@example.com>
2022-10-21 22:02:14 +0500

    localStorage is used in redux

--- src/redux/reducers/expenses.js
+++ src/redux/reducers/expenses.js
@@ -1,12 +1,26 @@
 import { expenseTypes } from "../action-types/expenses"
 
+const initialStateList=()=>{
+    //get data from local storage
+    const list = JSON.parse(localStorage.getItem("expense_list"))
+    //make an empty list
+    let expense=[];
+    //if data is get from local storage means if data present then assign data to expense arry
+    if(list){
+        expense=list
+    }
+    return expense
+}
+
 const initialState={
-    expenseList:[]
+    expenseList:initialStateList()
 }
 
 export const expenseReducer=(state=initialState,{type,payload})=>{
     switch(type){
         case expenseTypes.ADD_EXPENSE:
+            //set data mean store in local storage
+           localStorage.setItem('expense_list', JSON.stringify([payload,...state.expenseList]))
             //we allways change state according to the reducer state
             // As in that case initialState have a list if we want to add new data in list then
             //we will change means add new data in list as below
@@ -15,8 +29,10 @@ export const expenseReducer=(state=initialState,{type,payload})=>{
                 expenseList:[payload,...state.expenseList]
             }
         case expenseTypes.DELETE_EXPENSE:{
-            const updateList = state.expenseList.filter(item=> item.id!==payload.id)
 
+           
+            const updateList = state.expenseList.filter(item=> item.createdAt!==payload.createdAt)
+            localStorage.setItem('expense_list',JSON.stringify(updateList))
             return{
                 ...state,
                 expenseList:updateList

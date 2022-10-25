7eb3169c557c0132f4ac50a0cf83c8bf2173af63
MahtabBukhari <git config --global user.email you@example.com>
2022-10-25 17:08:20 +0500

    pagination in every project its one of pagination

--- src/components/ReactPagination.js
+++ src/components/ReactPagination.js
@@ -6,45 +6,82 @@ import ExpenseList from './expenseList';
 const ReactPagination = ({itemsPerPage}) => {
 
     const {expenseList:list,query} = useSelector(state=>state.expenses)
-    
     const filterList = list.filter(item=> item.title.includes(query))
 
+  const [currentPage, setCurrentPage]=useState(1)
+    // const [pageOffset, setPageOffset] = useState(0);
     const [currentItems, setCurrentItems] = useState([]);
     const [pageCount, setPageCount] = useState(0);
-    // Here we use item offsets; we could also use page offsets
-    // following the API or data you're working with.
-    const [itemOffset, setItemOffset] = useState(0);
+
+ 
   console.log(currentItems)
+
     useEffect(() => {
-      // Fetch items from another resources.
-      const endOffset = itemOffset + itemsPerPage;
-      console.log(`Loading items from ${itemOffset} to ${endOffset}`);
-      setCurrentItems(filterList.slice(itemOffset, endOffset));
+
+    //   const firstPageIndex = (currentPage - 1) * PageSize;
+    // const lastPageIndex = firstPageIndex + PageSize;
+    // return data.slice(firstPageIndex, lastPageIndex);
+
+      // const endOffset = pageOffset + itemsPerPage;
+
+        const firstPageIndex = (currentPage - 1) * itemsPerPage;
+        const lastPageIndex = firstPageIndex + itemsPerPage;
+
+      setCurrentItems(filterList.slice(firstPageIndex, lastPageIndex));
+
       setPageCount(Math.ceil(filterList.length / itemsPerPage));
-    }, [itemOffset, itemsPerPage,filterList]);
+    }, [itemsPerPage,currentPage]);
+
+ 
+   const handlePageChange = (event) => {
+      
+       console.log(event.selected)
+       if(event.selected++){
+        setCurrentPage(currentPage+1);
+        console.log(currentPage+1)
+
+       } else if(event.selected --){
+        setCurrentPage(currentPage-1);
+        console.log(currentPage-1)
+       }else{
+        console.log('abc')
+       }
+      
+       
   
-    // Invoke when user click to request another page.
-    const handlePageClick = (event) => {
-      const newOffset = (event.selected * itemsPerPage) % filterList.length;
-      console.log(
-        `User requested page number ${event.selected}, which is offset ${newOffset}`
-      );
-      setItemOffset(newOffset);
-    };
-  
+
+
+  };
+
     return (
       <>
       <ExpenseList currentItems={currentItems}/>
         {/* <Items currentItems={currentItems} /> */}
-        <ReactPaginate
+      <div style={{marginLeft:"25%",}}>
+      <ReactPaginate
+         
+          previousLabel="Previous"
+          nextLabel="Next"
+          pageClassName="page-item"
+          pageLinkClassName="page-link"
+          previousClassName="page-item"
+          previousLinkClassName="page-link"
+          nextClassName="page-item"
+          nextLinkClassName="page-link"
           breakLabel="..."
-          nextLabel="next >"
-          onPageChange={handlePageClick}
-          pageRangeDisplayed={5}
+          breakClassName="page-item"
+          breakLinkClassName="page-link"
           pageCount={pageCount}
-          previousLabel="< previous"
-          renderOnZeroPageCount={null}
-        />
+          marginPagesDisplayed={2}
+          pageRangeDisplayed={4}
+          onPageChange={handlePageChange}
+          containerClassName="pagination"
+          activeClassName="active"
+          // forcePage={pageOffset}
+      />
+        </div>  
+        
+      
       </>
     );
   }
